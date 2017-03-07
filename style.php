6945e75547a520d391cbb9ae7c1a8e17dbeef0f1
ecperry <ecperry17@gmail.com>
2017-03-07 12:59:41 -0500

    Added style guide placeholder.

---
+++ style.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<?php include 'header.php'; ?>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Style Guide</title>
+  </head>
+  <body>
+    <h1> Vidaloka </h1>
+    <h2> A B C D E F G H I J K L M </h2>
+    <h2> N O P Q R S T U V W X Y Z</h2>
+    <h2> ! @ # $ % ^ * . , ? ( ) -</h2>
+    <h1> Lato </h1>
+    <h2> A B C D E F G H I J K L M </h2>
+    <h2> N O P Q R S T U V W X Y Z</h2>
+    <h2> ! @ # $ % ^ * . , ? ( ) -</h2>
+    <br>
+    <h1> Header 1 </h1>
+    <h2> Header 2 </h2>
+    <h3> Header 3 </h3>
+    <h4> Header 4 </h4>
+    <h5> Header 5 </h5>
+    <br>
+    <p> p </p>
+    <p> p.lead </p>
+    <p> p.light </p>
+    <br>
+    <button class="btn btn-sm">small button</button>
+    <br>
+    <br>
+    <button class="btn btn-md">medium button</button>
+    <br>
+    <br>
+    <button class="btn btn-lg">large button</button
+  </body>
+</html>
--- update.php
+++ update.php
@@ -3,12 +3,12 @@ include 'database.php';
 $stmt = $db->prepare('UPDATE contacts SET title = :title, description = :description, priority = :priority, completed = :completed WHERE id = :id');
 //$stmt->bindParam
 $stmt->execute(array(
-  ':first' => $_POST['first'],
+  ':first' => array_key_exists('first', $_POST),
   ':last' => $_POST['last'],
   ':title' => $_POST['title'],
   ':address' => $_POST['address'],
   ':city' => $_POST['city'],
-  ':state' => $_POST['state'],
+  ':state' => array_key_exists('state', $_POST),
   ':zip' => $_POST['zip'],
   ':phone' => $_POST['phone'],
   ':notes' => $_POST['notes']
